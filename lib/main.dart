24330331ad9246943ba0d9327c53177c27e22412
kapil619 <kapilbadokar321@gmail.com>
2023-08-14 21:01:19 +0530

    Initial commit with a custom widget

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import "package:quizapp/start_screen.dart";
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: StartScreen(),
+    ),
+  ));
+}
